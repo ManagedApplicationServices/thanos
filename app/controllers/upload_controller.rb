499a285b36c53fe6db1eea52df72ffc2fd3ce004
Ken Tan <kentan0130@gmail.com>
2014-03-17 15:30:00 +0800

    working master

---
+++ app/controllers/upload_controller.rb
@@ -0,0 +1,51 @@
+require 'builder'
+
+class UploadController < ApplicationController
+  def create
+    file = params[:file]
+    filename = params[:filename].presence || File.basename(file.original_filename, ".*")
+
+    Spreadsheet.client_encoding = 'UTF-8'
+    spreadsheet = Spreadsheet.open file.open
+    xml = generate_xml(spreadsheet)
+    raise "DebitAmt and CreditAmt does not tally." if @total_debit.round(2) != @total_credit.round(2)
+    raise "HomeDebitAmt and HomeCreditAmt does not tally." if @total_home_debit.round(2) != @total_home_credit.round(2)
+
+    send_data(xml, type: "text/xml", filename: filename)
+  end
+
+  private
+  def generate_xml(spreadsheet)
+    @total_debit = 0
+    @total_credit = 0
+    @total_home_debit = 0
+    @total_home_credit = 0
+    builder = Builder::XmlMarkup.new(:target => "", :indent => 1)
+    builder.target!
+    sheet1 = spreadsheet.worksheet 0
+    builder.GLInterface do
+      sheet1.each_with_index do |row, index|
+        if index != 0
+          @total_debit += row[8].to_f
+          @total_credit += row[9].to_f
+          @total_home_debit += row[11].to_f
+          @total_home_credit += row[12].to_f
+
+          if all_columns_filled?(row)
+            builder.GLInterfaceRow do |b|
+              ENV["column_names"].split(",").each_with_index do |column, index|
+                b.__send__(column, row[index])
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def all_columns_filled?(row)
+    (0..12).to_a.each do |i|
+      return false if row[i].to_s.length <= 0
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thanos::Application.config.secret_key_base = 'd02e9c180915b6d0bf8f69d59834933ce1a207b29ce9175b65625cc552d868757047df7a32df07ce3ad5263409638de1a25c7a869911166b798edc0b03ea9e0d'
