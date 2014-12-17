653368af36d1ef7dfa1bca1f684590f2a561b5da
Sam Hon <coffeesam@users.noreply.github.com>
2014-12-18 03:18:40 +0800

    Updated secret_token.rb
    Avoid hard coding secret key base
    Recommends upgrading to Rails 4.1 and use secrets.yml instead

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Thanos::Application.config.secret_key_base = 'd02e9c180915b6d0bf8f69d59834933ce1a207b29ce9175b65625cc552d868757047df7a32df07ce3ad5263409638de1a25c7a869911166b798edc0b03ea9e0d'
+Thanos::Application.config.secret_key_base = ENV['SECRET_TOKEN']
